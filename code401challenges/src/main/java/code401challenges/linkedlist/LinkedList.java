b347f7aa99619f5de8af7cd343e50c667d74b1be
Peter Lee <leepj85@gmail.com>
2019-08-13 10:32:09 -0700

    code challenge 6 completed

--- code401challenges/src/main/java/code401challenges/linkedlist/LinkedList.java
+++ code401challenges/src/main/java/code401challenges/linkedlist/LinkedList.java
@@ -2,38 +2,85 @@ package code401challenges.linkedlist;
 
 public class LinkedList<T> {
 
-    private Node head = null;
+    private Node<T> head;
 
     public void insert(T value) {
         Node<T> newNode = new Node<>(value);
         if (head == null) {
             head = newNode;
         } else {
-            newNode.next = head;
+            newNode.setNextNode(head);
             head = newNode;
         }
     }
 
-    public boolean includes(T value) {
+    public void append(T value) {
+        Node<T> newNode = new Node<>(value);
         Node current = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (current.getNextNode() != null) {
+                current = current.getNextNode();
+            }
+            current.setNextNode(newNode);
+        }
+    }
+
+    public void insertBefore(T value, T newVal) {
+        Node<T> newNode = new Node<>(newVal);
+        Node current = head;
+        if (head == null) {
+            head = newNode;
+        } else if (current.getValue().equals(value)) {
+            newNode.setNextNode(current);
+            head = newNode;
+        } else {
+            while (!current.getNextNode().getValue().equals(value)) {
+                current = current.getNextNode();
+//                if ( current == null) {
+//                }
+            }
+            newNode.setNextNode(current.getNextNode());
+            current.setNextNode(newNode);
+        }
+    }
+
+    public void insertAfter(T value, T newVal) {
+        Node<T> newNode = new Node<>(newVal) ;
+        Node current = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (!current.getValue().equals(value)) {
+                current = current.getNextNode();
+            }
+            newNode.setNextNode(current.getNextNode());
+            current.setNextNode(newNode);
+        }
+
+    }
+
+    public boolean includes(T value) {
+        Node<T> current = head;
         boolean result = false;
         while (current != null) {
-            if (current.value.equals(value)) {
+            if (current.getValue().equals(value)) {
                 result = true;
                 break;
             }
-            current = current.next;
+            current = current.getNextNode();
         }
         return result;
     }
 
     public String toString() {
-        String results = "";
-        Node current = head;
+        String results = "head -->";
+        Node<T> current = head;
         while (current != null) {
-            results += current.value + " ";
-            current = current.next;
+            results += " " + current.getValue() + " -->";
+            current = current.getNextNode();
         }
-        return results;
+        return results + " end";
     }
 }
--- code401challenges/src/test/java/code401challenges/linkedlist/LinkedListTest.java
+++ code401challenges/src/test/java/code401challenges/linkedlist/LinkedListTest.java
@@ -6,17 +6,87 @@ import static org.junit.Assert.*;
 public class LinkedListTest {
 
     @Test
-    public void LinkedList(){
+    public void linkedList(){
         LinkedList<String> linkedList= new LinkedList<>();
 
         linkedList.insert("hi");
-        assertEquals("check if 'insert' and 'toString' methods work", "hi ", linkedList.toString() );
+        assertEquals("check if 'insert' and 'toString' methods work", "head --> hi --> end", linkedList.toString() );
 
         linkedList.insert("world");
         assertEquals("check if 'insert' method adds to the beginning of LinkedList",
-                "world hi ", linkedList.toString());
+                "head --> world --> hi --> end", linkedList.toString());
 
         assertTrue("check if 'includes' method returns true when value exists in LinkedList", linkedList.includes("world"));
         assertFalse("check if 'includes' method returns false when value does not exist in LinkedList", linkedList.includes("blob"));
     }
+
+    @Test
+    public void linkedListInsertion() {
+
+        LinkedList<Integer> testSingleNode = new LinkedList<>();
+        testSingleNode.append(1);
+        assertEquals ("check if 'append' method adds node to an empty LinkedList",
+                "head --> 1 --> end", testSingleNode.toString());
+
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.append(1);
+        ll.append(3);
+        ll.append(2);
+        ll.append(5);
+        assertEquals("check if 'append' method adds to the end of a LinkedList",
+                "head --> 1 --> 3 --> 2 --> 5 --> end", ll.toString());
+
+        LinkedList<Integer> llInsertBefore1 = new LinkedList<>();
+        llInsertBefore1.append(1);
+        llInsertBefore1.append(3);
+        llInsertBefore1.append(2);
+        llInsertBefore1.insertBefore(3, 5);
+        assertEquals("add 5 before 3 using 'insertBefore()'",
+                "head --> 1 --> 5 --> 3 --> 2 --> end", llInsertBefore1.toString());
+
+        LinkedList<Integer> llInsertBefore2 = new LinkedList<>();
+        llInsertBefore2.append(1);
+        llInsertBefore2.append(3);
+        llInsertBefore2.append(2);
+//        System.out.println(llInsertBefore2.toString());
+        llInsertBefore2.insertBefore(1,5);
+        assertEquals("add 5 before 1 using 'insertBefore()'",
+                "head --> 5 --> 1 --> 3 --> 2 --> end", llInsertBefore2.toString());
+
+        LinkedList<Integer> llInsertBefore3 = new LinkedList<>();
+        llInsertBefore3.append(1);
+        llInsertBefore3.append(2);
+        llInsertBefore3.append(2);
+        llInsertBefore3.insertBefore(2, 5);
+        assertEquals("add 5 before 2 using 'insertBefore()'",
+                "head --> 1 --> 5 --> 2 --> 2 --> end", llInsertBefore3.toString());
+
+//        LinkedList<Integer> llInsertBefore4 = new LinkedList<>();
+//        llInsertBefore4.append(1);
+//        llInsertBefore4.append(3);
+//        llInsertBefore4.append(2);
+//        System.out.println(llInsertBefore4.toString());
+//        llInsertBefore4.insertBefore(4, 5);
+//        System.out.println(llInsertBefore4.toString());
+
+
+        LinkedList<Integer> insertAfterTest1 = new LinkedList<>();
+        insertAfterTest1.append(1);
+        insertAfterTest1.append(3);
+        insertAfterTest1.append(2);
+        insertAfterTest1.insertAfter(3, 5);
+        assertEquals("add 5 after 3 using 'insertAfter()'",
+                "head --> 1 --> 3 --> 5 --> 2 --> end", insertAfterTest1.toString());
+
+
+        LinkedList<Integer> insertAfterTest2 = new LinkedList<>();
+        insertAfterTest2.append(1);
+        insertAfterTest2.append(3);
+        insertAfterTest2.append(2);
+        insertAfterTest2.insertAfter(2, 5);
+        assertEquals("add 5 after 2 using 'insertAfter()'",
+                "head --> 1 --> 3 --> 2 --> 5 --> end", insertAfterTest2.toString());
+    }
+
+
 }
