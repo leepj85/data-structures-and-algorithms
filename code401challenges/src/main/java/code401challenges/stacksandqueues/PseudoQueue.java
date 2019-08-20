a10f5571656ff23873be3b74c6c66ef7057fa574
Peter Lee <leepj85@gmail.com>
2019-08-20 09:37:08 -0700

    completed code challenge 11

---
+++ code401challenges/src/main/java/code401challenges/stacksandqueues/PseudoQueue.java
@@ -0,0 +1,26 @@
+package code401challenges.stacksandqueues;
+
+public class PseudoQueue<T> {
+
+    Stack<T> helperStack = new Stack<>();
+    Stack<T> pseudoQueue = new Stack<>();
+
+    //First-in, first-out approach
+
+    public void enqueue(T value) {
+        helperStack.push(value);
+    }
+
+    public T dequeue() {
+        while(helperStack.top != null) {
+            pseudoQueue.push(helperStack.pop());
+        }
+        T result = pseudoQueue.pop();
+        pseudoQueue.push(result);
+        while(pseudoQueue.top != null) {
+            helperStack.push(pseudoQueue.pop());
+        }
+        return result;
+    }
+
+}
---
+++ code401challenges/src/test/java/code401challenges/stacksandqueues/PseudoQueueTest.java
@@ -0,0 +1,27 @@
+package code401challenges.stacksandqueues;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PseudoQueueTest {
+
+    Queue<Integer> queue = new Queue<>();
+    PseudoQueue<Integer> pseudoQueue = new PseudoQueue<>();
+
+    @Test
+    public void enqueueAndDequeueTest() {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        pseudoQueue.enqueue(1);
+        pseudoQueue.enqueue(2);
+        pseudoQueue.enqueue(3);
+//        System.out.println(pseudoQueue.dequeue());
+        assertTrue(queue.dequeue().equals(pseudoQueue.dequeue()));
+    }
+
+
+
+}
